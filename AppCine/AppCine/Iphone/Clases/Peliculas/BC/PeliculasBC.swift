ff53a79545402e0db3bb4c88f6574f720c0267ac
B303-xx <administrador@b303-24.local>
2016-10-27 09:41:43 -0500

    --

---
+++ AppCine/AppCine/Iphone/Clases/Peliculas/BC/PeliculasBC.swift
@@ -0,0 +1,13 @@
+//
+//  PeliculasBC.swift
+//  AppCine
+//
+//  Created by B303-24 on 27/10/16.
+//  Copyright © 2016 B303-24. All rights reserved.
+//
+
+import UIKit
+
+class PeliculasBC: NSObject {
+
+}
